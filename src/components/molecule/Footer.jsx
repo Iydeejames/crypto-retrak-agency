102bfcf635eb31188fdf5dbaca4a03180e658ddb
Idaraobong James <iydeejaymes@gmail.com>
2024-12-21 13:44:25 +0100

    Update Footer.jsx

--- src/components/molecule/Footer.jsx
+++ src/components/molecule/Footer.jsx
@@ -7,13 +7,11 @@ const Footer = () => {
       {/* Top Horizontal Line */}
       <div className="w-5/6 mx-auto border-t-2 border-white"></div>
 
-
       {/* Top Section (Contact Information) */}
       <div className="py-8 px-4 md:px-8 w-full">
         <div className="container mx-auto px-4 md:px-8">
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-8">
-            {/* Contact Information */}
-            <div>
+          <div className="flex justify-center">
+            <div className="flex flex-col items-center text-center">
               <h4 className="font-bold mb-4 text-xs md:text-base">Contact Information</h4>
               <ul className="text-xs md:text-base">
                 <li>+1 (757) 235-2717</li>
@@ -21,24 +19,6 @@ const Footer = () => {
                 <li>1190 Big Elm, Kansas City, Missouri, USA</li>
               </ul>
             </div>
-
-            {/* Our Socials Section 
-            <div>
-              <h4 className="font-bold mb-4 text-xs md:text-base">Our Socials</h4>
-              <div className="flex space-x-4">
-                <a href="#" target="_blank" rel="noopener noreferrer" className="text-blue-600">
-                  <FaFacebookF size={22} />
-                </a>
-                <a href="#" target="_blank" rel="noopener noreferrer" className="text-pink-600">
-                  <FaInstagram size={22} />
-                </a>
-                <a href="#" target="_blank" rel="noopener noreferrer" className="text-green-500">
-                  <FaWhatsapp size={22} />
-                </a>
-              </div>
-            </div>
-            */}
-          
           </div>
         </div>
       </div>
