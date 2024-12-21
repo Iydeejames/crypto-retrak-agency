6e85ab60e124d63624e56b384afd52a1ec92acde
Idaraobong James <iydeejaymes@gmail.com>
2024-12-21 16:55:17 +0100

    Update Blog.jsx

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -29,7 +29,7 @@ const Blog = () => {
     <div className="min-h-screen bg-gray-50 flex flex-col items-center py-8 px-4">
 
       {/* Main Heading */}
-      <h1 className="text-4xl font-bold text-gray-800 mb-6 text-center">Crypto Tracking & Recovery Blog</h1>
+      <h1 className=" text-lg sm:text-3xl font-bold text-gray-800 mb-6 text-center">Crypto Tracking & Recovery Blog</h1>
 
       {/* Subtext */}
       <p className="text-gray-600 text-center max-w-4xl mb-8">
@@ -79,7 +79,7 @@ const Blog = () => {
           <ul className="space-y-6">
             {posts.map((post, index) => (
               <li key={index} className="border-b pb-4">
-                <a href={post.link} className="text-xl text-blue-600 hover:underline">{post.title}</a>
+                <a href={post.link} className="text-lg text-blue-600 hover:underline">{post.title}</a>
                 <p className="text-gray-500 text-sm">{post.date}</p>
               </li>
             ))}
